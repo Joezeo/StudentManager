47733adb8519fa25752eaa876290f10af8a44786
Joezeo <1252795700@qq.com>
2019-07-23 10:03:10 +0800

    完成服务器端对学生客户端请求的处理功能，版本1.0完成

--- SM_Server/src/com/joezeo/opration/SOpration.java
+++ SM_Server/src/com/joezeo/opration/SOpration.java
@@ -1,5 +1,10 @@
 package com.joezeo.opration;
 
+import com.joezeo.core.Query;
+import com.joezeo.core.QueryFactory;
+import com.joezeo.po.T_sreginfo;
+import com.joezeo.po.T_student;
+
 /**
  * 学生端操作实现类
  */
@@ -11,10 +16,41 @@ public class SOpration extends Opration {
 
     @Override
     public void login(Integer id, String pwd) {
+        //从数据库表t_sreginfo获取该id的正确密码
+        Query query = QueryFactory.getQuery();
+        String sql = "SELECT pwd FROM t_sreginfo WHERE teaId=?";
+        String cPwd = (String)query.queryValue(sql, new Object[]{id});
+
+        if(pwd.equals(cPwd)){
+            msg.setIsLogin("true"); //表示登陆成功
+        } else {
+            msg.setIsLogin("false");//表示登陆失败
+        }
+
+        msg.setInquiryContent(null);
+        msg.setIsRegister(null);
     }
 
     @Override
     public void register(Integer id, String pwd, String name) {
+        //先从t_student表中获取id、name
+        //判断是否与用户输入的信息匹配
+        //如匹配则注册成功 否则注册失败
+        Query query = QueryFactory.getQuery();
+        String sql = "SELECT * FROM t_student WHERE stuId=?";
+        T_student ts = (T_student) query.queryRow(T_student.class, sql, new Object[]{id});
+
+        if(ts.getName().equals(name)){
+            T_sreginfo sInfo = new T_sreginfo();
+            sInfo.setStuId(id);
+            sInfo.setPwd(pwd);
+            sInfo.setName(name);
+
+            query.insert(sInfo);
+            msg.setIsRegister("true");
+        } else {
+            msg.setIsRegister("false");
+        }
     }
 
     @Override
@@ -24,7 +60,14 @@ public class SOpration extends Opration {
 
     @Override
     public void inquiry(Integer id) {
+        String sql = "SELECT * from t_student WHERE stuId=?";
 
+        Query query = QueryFactory.getQuery();
+        T_student value = (T_student)query.queryRow(T_student.class, sql, new Object[]{id});
+
+        msg.setInquiryContent(value.getName() + "：" + value.getScore());
+        msg.setIsLogin(null);
+        msg.setIsRegister(null);
     }
 
     public SOpration() {
